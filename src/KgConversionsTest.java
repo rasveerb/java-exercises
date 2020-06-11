6cee37131cd9c0e11c291ce0f9813c44ae5158c4
rasveerb <rasveerb@sky.com>
2020-06-11 11:38:46 +0100

    refactored test file to remove repeated code

--- src/KgConversionsTest.java
+++ src/KgConversionsTest.java
@@ -5,59 +5,61 @@ import static org.junit.Assert.*;
 
 public class KgConversionsTest {
 
+    double kg = 0.45359237;
+
     @Test
     public void tonToKg() {
         KgConversions testConverter = new KgConversions();
 
-       assertEquals(1016.05, testConverter.tonToKg(2240, 0.45359237),0);
+       assertEquals(1016.05, testConverter.tonToKg(2240, kg),0);
     }
 
     @Test
     public void hundredweightToKg(){
         KgConversions testConverter = new KgConversions();
 
-        assertEquals(50.8, testConverter.hundredWeightToKg(112, 0.45359237), 0);
+        assertEquals(50.8, testConverter.hundredWeightToKg(112, kg), 0);
     }
 
     @Test
     public void quarterToKg(){
         KgConversions testConverter = new KgConversions();
 
-        assertEquals(12.7, testConverter.quarterToKg(28, 0.45359237), 0);
+        assertEquals(12.7, testConverter.quarterToKg(28, kg), 0);
     }
 
     @Test
     public void stoneToKg() {
         KgConversions testConverter = new KgConversions();
 
-        assertEquals(6.35, testConverter.stoneToKg(14,0.45359237), 0);
+        assertEquals(6.35, testConverter.stoneToKg(14, kg), 0);
     }
 
     @Test
     public void ounceToKg() {
         KgConversions testConverter = new KgConversions();
 
-        assertEquals(0.03, testConverter.ounceToKg(0.0625,0.45359237), 0);
+        assertEquals(0.03, testConverter.ounceToKg(0.0625, kg), 0);
     }
 
     @Test
     public void stonesAndOuncesToKg(){
         KgConversions testConverter = new KgConversions();
 
-        assertEquals(72.57, testConverter.stonesAndOuncesToKg(11,6, 0.45359237), 0);
+        assertEquals(72.57, testConverter.stonesAndOuncesToKg(11,6, kg), 0);
     }
 
     @Test
     public void drachmToKg() {
         KgConversions testConverter = new KgConversions();
 
-        assertEquals(0, testConverter.drachmToKg(0.00390625,0.45359237), 0);
+        assertEquals(0, testConverter.drachmToKg(0.00390625, kg), 0);
     }
 
     @Test
     public void grainToKg() {
         KgConversions testConverter = new KgConversions();
 
-        assertEquals(0, testConverter.grainToKg(0.00014285714,0.45359237), 0);
+        assertEquals(0, testConverter.grainToKg(0.00014285714, kg), 0);
     }
 }
