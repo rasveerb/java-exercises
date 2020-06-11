78f6fbcfec7b9e1b1d80ceab7a309ab4b290ac35
rasveerb <rasveerb@sky.com>
2020-06-11 11:37:17 +0100

    added tests and refactored

--- src/KgConversions.java
+++ src/KgConversions.java
@@ -1,40 +1,63 @@
 /* This class takes imperial measurements and converts them into Kilograms*/
 public class KgConversions {
-    double kg = 0.45359237;
 
-    public static double tonToKg(double kg){
-        double ton = 2240;
-        return(ton * kg);
+    public static double tonToKg(double tons, double kg){
+        double weight = (tons * kg);
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return(roundedWeight);
     }
 
-    public static double hundredWeightToKg(double kg){
-        double hundredWeight = 112;
-        return(hundredWeight * kg);
+    public static double hundredWeightToKg(double hundredWeights, double kg){
+        double weight = (hundredWeights * kg);
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return(roundedWeight);
     }
 
-    public static double quarterToKg(double kg){
-        double quarter = 28;
-        return(quarter * kg);
+    public static double quarterToKg(double quarters, double kg){
+        double weight = (quarters * kg);
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return(roundedWeight);
     }
 
-    public static double stoneToKg(double kg){
-        double stone = 14;
-        return(stone * kg);
+    public static double stoneToKg(double stones, double kg){
+        double weight = (stones * kg);
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return(roundedWeight);
     }
 
-    public static double ounceToKg(double kg){
-        double ounce = 0.0625;
-        return (ounce * kg);
+    public static double ounceToKg(double ounces, double kg){
+        double weight = (ounces * kg);
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return (roundedWeight);
     }
 
-    public static double drachmToKg(double kg){
-        double drachm = 0.00390625;
-        return (drachm * kg);
+    public static double stonesAndOuncesToKg(double stones, double pounds, double kg){
+        double totalPounds = ((stones * 14)+pounds);
+
+        double weight = (totalPounds * kg );
+
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return (roundedWeight);
     }
 
-    public static double grainToKg(double kg){
-        double grain = 0.00014285714;
-        return (grain * kg);
+    public static double drachmToKg(double drachms, double kg){
+        double weight = (drachms * kg);
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return (roundedWeight);
+    }
+
+    public static double grainToKg(double grains, double kg){
+        double weight = (grains * kg);
+        double roundedWeight = (double) Math.round(weight * 100) / 100;
+
+        return (roundedWeight);
     }
 
     public static void main(String[] args){
--- src/KgConversionsTest.java
+++ src/KgConversionsTest.java
@@ -1,4 +1,63 @@
+import org.junit.Assert;
 import org.junit.Test;
+import java.math.BigDecimal;
+import static org.junit.Assert.*;
 
 public class KgConversionsTest {
+
+    @Test
+    public void tonToKg() {
+        KgConversions testConverter = new KgConversions();
+
+       assertEquals(1016.05, testConverter.tonToKg(2240, 0.45359237),0);
+    }
+
+    @Test
+    public void hundredweightToKg(){
+        KgConversions testConverter = new KgConversions();
+
+        assertEquals(50.8, testConverter.hundredWeightToKg(112, 0.45359237), 0);
+    }
+
+    @Test
+    public void quarterToKg(){
+        KgConversions testConverter = new KgConversions();
+
+        assertEquals(12.7, testConverter.quarterToKg(28, 0.45359237), 0);
+    }
+
+    @Test
+    public void stoneToKg() {
+        KgConversions testConverter = new KgConversions();
+
+        assertEquals(6.35, testConverter.stoneToKg(14,0.45359237), 0);
+    }
+
+    @Test
+    public void ounceToKg() {
+        KgConversions testConverter = new KgConversions();
+
+        assertEquals(0.03, testConverter.ounceToKg(0.0625,0.45359237), 0);
+    }
+
+    @Test
+    public void stonesAndOuncesToKg(){
+        KgConversions testConverter = new KgConversions();
+
+        assertEquals(72.57, testConverter.stonesAndOuncesToKg(11,6, 0.45359237), 0);
+    }
+
+    @Test
+    public void drachmToKg() {
+        KgConversions testConverter = new KgConversions();
+
+        assertEquals(0, testConverter.drachmToKg(0.00390625,0.45359237), 0);
+    }
+
+    @Test
+    public void grainToKg() {
+        KgConversions testConverter = new KgConversions();
+
+        assertEquals(0, testConverter.grainToKg(0.00014285714,0.45359237), 0);
+    }
 }
