119d096c0da176f099a19fed57416624e1325454
rasveerb <rasveerb@sky.com>
2020-06-18 11:35:00 +0100

    removed comments

--- src/InterestConverter/CapitalInterest.java
+++ src/InterestConverter/CapitalInterest.java
@@ -21,14 +21,5 @@ public class CapitalInterest {
 
     }
 
-//    A capital of £ 100 is invested at a fixed interest rate of 2.3 per cent.
-//    The interest is added to the capital at the end of each year.
-//    Use the formula
-//            total = capital ∗ (1 + 0.01 ∗ interest rate)^years
-//    print the balance after each of the first 5 years
-//    and after 500 years.
-
-    public static void main(String[] args){
-System.out.println(interestCalculator(100, 2.3, 5));
-    }
+    public static void main(String[] args){}
 }
