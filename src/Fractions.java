26ccb425601c918a31649a00a3a7ce36a69e9bf9
rasveerb <rasveerb@sky.com>
2020-06-19 18:20:04 +0100

    added sum and product methods

---
+++ src/Fractions.java
@@ -0,0 +1,30 @@
+/* A class which calculates the addition and multiplication of fractions.*/
+
+public class Fractions {
+    int numerator1;
+    int denominator1;
+    //below = positive ints only - TODO: put in check for this
+    int numerator2;
+    int denominator2;
+
+    public static String fractionSum(int numerator1, int denominator1, int numerator2, int denominator2) {
+        int sumFractionTop = (((numerator1 * denominator2)+(denominator1 * numerator2)));
+        int sumFractionBottom = ((denominator1 * denominator2));
+
+        String total = sumFractionTop + "/" + sumFractionBottom;
+
+        return total;
+    }
+
+    public static String fractionProduct(int numerator1, int denominator1, int numerator2, int denominator2){
+        int productFractionTop = (numerator1 * numerator2);
+        int productFractionBottom = (denominator1 * denominator2);
+
+        String total = productFractionTop + "/" + productFractionBottom;
+
+        return total;
+    }
+
+    public static void main(String[] args){}
+
+}
---
+++ src/Fractions/FractionsTest.java
@@ -0,0 +1,10 @@
+package Fractions;
+
+// Test your program for:
+// 1/2 + 1/3
+//1/3 + 3/4
+//1/2 * 2/3
+//1/4 * 2/3
+
+public class FractionsTest {
+}
