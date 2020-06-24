45c00340dad6d009ca9ec13c1440ecc7e7a3b67a
rasveerb <rasveerb@sky.com>
2020-06-24 10:23:03 +0100

    began adding methods

---
+++ src/ClockAngles.java
@@ -0,0 +1,11 @@
+/* This class will calculate the angle between two analog clock hands at any given time. */
+public class ClockAngles {
+
+    public static double angles (int hour, int minutes){
+        return(hour + minutes); //TODO replace this dummy method
+    }
+
+    public static void main (String[] args){
+
+    }
+}
