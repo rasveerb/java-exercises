106f66df613b3cf286659a6860f66b2605b50927
rasveerb <rasveerb@sky.com>
2020-06-24 13:28:03 +0100

    removed unused declarations

--- src/Fractions/Fractions.java
+++ src/Fractions/Fractions.java
@@ -1,11 +1,6 @@
 package Fractions;/* A class which calculates the addition and multiplication of fractions.*/
 
 public class Fractions {
-    int numerator1;
-    int denominator1;
-    //below = positive ints only - TODO: put in check for this
-    int numerator2;
-    int denominator2;
 
     public static String fractionSum(int numerator1, int denominator1, int numerator2, int denominator2) {
         int sumFractionTop = (((numerator1 * denominator2)+(denominator1 * numerator2)));
