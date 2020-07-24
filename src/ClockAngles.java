e60c1958556efebfd053d880b302fa330a4737d6
rasveerb <rasveerb@sky.com>
2020-07-24 17:43:46 +0100

    WIP fix 15mins to be 90 not 270

--- src/ClockAngles.java
+++ src/ClockAngles.java
@@ -1,11 +1,28 @@
 /* This class will calculate the angle between two analog clock hands at any given time. */
 public class ClockAngles {
 
-    public static double angles (int hour, int minutes){
-        return(hour + minutes); //TODO replace this dummy method
+    public static double clockwiseAngles (int hour, int minutes){
+        double hourAngle = (hour * 30);
+        double minuteAngle = (minutes * 6);
+
+        return (hourAngle - minuteAngle);
+    }
+
+    public static  double anticlockwiseAngles (int hour, int minutes){
+        return(0);
     }
 
     public static void main (String[] args){
+
+        System.out.println(clockwiseAngles(3,0));
+
+        System.out.println(clockwiseAngles(9,0));
+
+        System.out.println(clockwiseAngles(6,30));
+
+        System.out.println(clockwiseAngles(12,15));
+
+        System.out.println(clockwiseAngles(12,45));
 
     }
 }
